dc5a69a1f209486302fef8719c46edc43a580321
hafsa <hafsaalisharifyusuf@gmail.com>
2026-01-28 07:39:52 -0800

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+});document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('#create-task-form');
+  const input = document.querySelector('#new-task-description');
+  const taskList = document.querySelector('#tasks');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const li = document.createElement('li');
+    li.textContent = input.value;
+    taskList.appendChild(li);
+
+    input.value = '';
+  });
 });
